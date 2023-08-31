daf0cc03ea46f91ee735c6c0f1452a2c9981baa7
TheRedBlueCube3 <theredbluecube2@gmail.com>
2023-08-31 14:49:34 +0300

    removed test files

--- index.js
+++
@@ -1,8 +0,0 @@
-import DB from './DB.js';
-
-const database = new DB('data.tdb');
-
-database.write(null, 'thingy', 'two');
-database.write('head', 'thingy', 'three');
-
-console.log(database.read(null, 'thingy', 'two'));
